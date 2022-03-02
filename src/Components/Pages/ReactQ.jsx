73db635136e8d2e1a603f2bb7c7c8502c9eba9ff
nub69 <shabik.ashab@gmail.com>
2022-03-02 13:25:56 +0600

    answer for 15 react question added

--- src/Components/Pages/ReactQ.jsx
+++ src/Components/Pages/ReactQ.jsx
@@ -1,79 +1,108 @@
-import { TextField, Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import Navbar from '../Navbar'
-
+import { Checkbox, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Navbar from "../Navbar";
 
 const ReactQ = () => {
-    const [Questions, setQuestions] = useState([]);
-    const [displayQuestion, setDisplayQuestions] = useState([]);
-    // setReactQuestion(reactQ);
-    
-    useEffect(() => {
-        fetch('./reactQ.json')
-            .then(res => res.json())
-            .then(data => {
-                setDisplayQuestions(data);
-                setQuestions(data)
-            });
-    }, []);
+  const [Questions, setQuestions] = useState([]);
+  const [displayQuestion, setDisplayQuestions] = useState([]);
+  // setReactQuestion(reactQ);
 
-    console.log(Questions);
-    const handleSearch = event => {
-        const searchText = event.target.value;
+  const [checked, setChecked] = React.useState(true);
 
-        const matchedProducts = Questions.filter(product => product.q.toLowerCase().includes(searchText.toLowerCase()));
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
-        setDisplayQuestions(matchedProducts);
-    }
+  useEffect(() => {
+    fetch("./reactQ.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setDisplayQuestions(data);
+        setQuestions(data);
+      });
+  }, []);
+
+  console.log(Questions);
+  const handleSearch = (event) => {
+    const searchText = event.target.value;
+
+    const matchedProducts = Questions.filter((product) =>
+      product.q.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    setDisplayQuestions(matchedProducts);
+  };
 
   return (
     <>
-    <Navbar />
-    <Box sx={{mt:5,ml:10}}>
-        <Typography variant="h4">
-         React Question
-        </Typography>
+      <Navbar />
+      <Box sx={{ mt: 5, ml: 10 }}>
+        <Typography variant="h4">React Question</Typography>
         <Typography>
-        <Box sx={{ml:1}}>
-        <a target="_blank" href="https://github.com/Ebazhanov/linkedin-skill-assessments-quizzes/blob/master/react/reactjs-quiz.md">Visit Link for Answer</a>
-        </Box>
+          <Box sx={{}}>
+            <a
+              target="_blank"
+              href="https://github.com/Ebazhanov/linkedin-skill-assessments-quizzes/blob/master/react/reactjs-quiz.md"
+            >
+              Visit Link for Answer
+            </a>
+          </Box>
         </Typography>
-        <Box sx={{m:2}}>
-        <TextField id="outlined-search" label="Search Questions" 
-        onChange={handleSearch}
-        type="search" />
-        {/* <input
+        {/* search */}
+        <Box sx={{ mt: 2 }}>
+          <TextField
+            id="outlined-search"
+            label="Search Questions"
+            onChange={handleSearch}
+            type="search"
+            sx={{
+              width: "50%",
+            }}
+          />
+          {/* <input
                     type="text"
                     onChange={handleSearch}
                     placeholder="Search Question" /> */}
         </Box>
-        <Box sx={{mt:3}}>
-            {
-                displayQuestion.map((question) => (
-                    <Box sx={{mb:3}}>
-                     <Box sx={{m:1}} key={question.id}>
-                        {question.q}
-                    </Box >
-                    {
-                        question.codeImg &&
-                        <Box sx={{maxHeight:'30vh',maxWidth:'70vh'}}>
-                            <img src={ question.codeImg} alt="code image" />
-                        </Box>
-                    }
-                    </Box>
-                 
-                ))
-            }
-        </Box>
-   
-    </Box>
-   
-    
-    </>
-  )
-}
+        <Box sx={{ mt: 3 }}>
+          {displayQuestion.map((question) => (
+            <Box sx={{ mb: 3 }}>
+              <Box sx={{ m: 1 }} key={question.id}>
+                <Typography sx={{ fontSize: "1.1em" }}>{question.q}</Typography>
+              </Box>
+              {question.codeImg && (
+                <Box sx={{ maxHeight: "30vh", maxWidth: "70vh" }}>
+                  <img src={question.codeImg} alt="code image" />
+                </Box>
+              )}
+              {/* answer for question */}
+               <Box>
+                    {question.ansImg? 
+                <Box sx={{ maxHeight: "30vh", maxWidth: "70vh" }}>
+                  <img src={question.ansImg} alt="code image" />
+                </Box>
+                :
+                <>
+                <Checkbox
+                    checked={checked}
+                    onChange={handleChange}
+                    inputProps={{ "aria-label": "controlled" }}
+                  />
+                  {question.ans}
+                </>
+                  }
+            </Box>
+            </Box>
 
-export default ReactQ
+           
+          ))}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default ReactQ;
--- src/Data/React.js
+++ src/Data/React.js
@@ -1,71 +1,86 @@
 const react = [
     {
         id: 1,
-        q: "Q1. If you want to import just the Component from the React library, what syntax do you use?"
+        q: "Q1. If you want to import just the Component from the React library, what syntax do you use?",
+        ans:"import { Component } from 'react'"
     },
     {
         id: 2,
-        q: "Q2. If a function component should always render the same way given the same props, what is a simple performance optimization available for it?"
+        q: "Q2. If a function component should always render the same way given the same props, what is a simple performance optimization available for it?",
+        ans:'Wrap it in the React.memo higher-order component.'
     },
     {
         id: 3,
         q: "Q3. How do you fix the syntax error that results from running this code?",
-        codeImg:"https://i.ibb.co/DrFgxcH/Screenshot-from-2022-03-02-12-05-51.png"
+        codeImg:"https://i.ibb.co/DrFgxcH/Screenshot-from-2022-03-02-12-05-51.png",
+        ans:'Wrap the object in parentheses.'
     },
     {
         id: 4,
         q: "Q4. If you see the following import in a file, what is being used for state management in the component?",
-        codeImg:"https://i.ibb.co/3krMCd9/Screenshot-from-2022-03-02-12-07-03.png"
+        codeImg:"https://i.ibb.co/3krMCd9/Screenshot-from-2022-03-02-12-07-03.png",
+        ans:'React Hooks'
     },
     {
         id: 5,
         q: "Q5. Using object literal enhancement, you can put values back into an object. When you log person to the console, what is the output?",
-        codeImg:"https://i.ibb.co/gR8QCrq/Screenshot-from-2022-03-02-12-08-05.png"
+        codeImg:"https://i.ibb.co/gR8QCrq/Screenshot-from-2022-03-02-12-08-05.png",
+        ansImg:'https://i.ibb.co/R4H9nsH/Screenshot-from-2022-03-02-13-16-09.png'
     },
     {
         id: 6,
-        q: "Q6. What is the testing library most often associated with React?"
+        q: "Q6. What is the testing library most often associated with React?",
+        ans:'Jest'
     },
     {
         id: 7,
         q: 'Q7. To get the first item from the array (cooking) using array destructuring, how do you adjust this line?',
-        codeImg:'https://i.ibb.co/h25SMwf/Screenshot-from-2022-03-02-12-43-46.png'
+        codeImg:'https://i.ibb.co/h25SMwf/Screenshot-from-2022-03-02-12-43-46.png',
+        ans:'const [first] = ["cooking", "art", "history"]'
     },
     {
         id: 8,
-        q: "Q8. How do you handle passing through the component tree without having to pass props down manually at every level?"
+        q: "Q8. How do you handle passing through the component tree without having to pass props down manually at every level?",
+        ans:'React Context'
     },
     {
         id: 9,
         q: "Q9. What should the console read when the following code is run?",
-        codeImg:'https://i.ibb.co/2ggKbpt/Screenshot-from-2022-03-02-12-45-01.png'
+        codeImg:'https://i.ibb.co/2ggKbpt/Screenshot-from-2022-03-02-12-45-01.png',
+        ans:'Cat'
     },
     {
         id: 10,
-        q: "10. What is the name of the tool used to take JSX and turn it into createElement calls? "
+        q: "10. What is the name of the tool used to take JSX and turn it into createElement calls? ",
+        ans:'Babel'
     },
     {
         id: 11,
-        q: "11. Why might you use useReducer over useState in a React component? "
+        q: "11. Why might you use useReducer over useState in a React component? ",
+        ans:' when you need to manage more complex state in an app'
     },
     {
         id: 12,
         q: "12. Which props from the props object is available to the component with the following syntax?",
-        codeImg:'https://i.ibb.co/0B8FtYc/Screenshot-from-2022-03-02-12-46-07.png'
+        codeImg:'https://i.ibb.co/0B8FtYc/Screenshot-from-2022-03-02-12-46-07.png',
+        ans:'all of them'
     },
     {
         id: 13,
         q: "13. Consider the following code from React Router. What do you call :id in the path prop? ",
-        codeImg:'https://i.ibb.co/TLMXR2M/Screenshot-from-2022-03-02-12-47-00.png'
+        codeImg:'https://i.ibb.co/TLMXR2M/Screenshot-from-2022-03-02-12-47-00.png',
+        ans:"This is a route parameter"
     },
     {
         id: 14,
         q: "14. If you created a component called Dish and rendered it to the DOM, what type of element would be rendered? ",
-        codeImg:"https://i.ibb.co/P6Q0D31/Screenshot-from-2022-03-02-11-59-28.png"
+        codeImg:"https://i.ibb.co/P6Q0D31/Screenshot-from-2022-03-02-11-59-28.png",
+        ans:'h1'
     },
     {
         id: 15,
         q: "15. What does this React element look like given the following function? (Alternative: Given the following code, what does this React element look like?) ",
-        codeImg:'https://i.ibb.co/D7CZX0G/Screenshot-from-2022-03-02-12-47-45.png'
+        codeImg:'https://i.ibb.co/D7CZX0G/Screenshot-from-2022-03-02-12-47-45.png',
+        ansImg:'https://i.ibb.co/N73X63q/Screenshot-from-2022-03-02-13-04-52.png'
     },
 ]
